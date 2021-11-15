efd9f2b418fd5ed3df277547f26a00c0a23ab404
Derek <coveydrk@gmail.com>
2021-11-15 23:45:59 +0100

    fix: fix of next-config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-
-
-
-const nextTranslate = require("next-translate")
-module.exports = {
-  reactStrictMode: true,
-  ...nextTranslate()
-}
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withBundleAnalyzer({})
+const nextTranslate = require("next-translate")
+module.exports = {
+  reactStrictMode: true,
+  ...nextTranslate(),
+  ...withBundleAnalyzer({})
+}
