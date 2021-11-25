3dfe08c6eb92b23e34660d6dae0d116c60d21db7
Sarvar Tojikulov <sarvar3099@gmail.com>
2021-11-25 15:30:40 +0100

    burger menu

---
+++ components/Burger.tsx
@@ -0,0 +1,49 @@
+import classNames from "classnames";
+import React, { useState } from "react";
+
+interface BurgerProps {
+  status: boolean;
+  toggler: () => void;
+}
+
+const Burger: React.FC<BurgerProps> = ({ toggler, status }) => {
+  return (
+    <div className="relative sm:max-w-xl mx-auto">
+      <nav>
+        <div
+          className="text-gray-500 w-10 h-10 relative focus:outline-none"
+          onClick={toggler}
+        >
+          <span className="sr-only">Open main menu</span>
+          <div className="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
+            <span
+              aria-hidden="true"
+              className={classNames(
+                "block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out",
+                { "rotate-45": status },
+                { "-translate-y-1.5": !status }
+              )}
+            ></span>
+            <span
+              aria-hidden="true"
+              className={classNames(
+                "block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out",
+                { "opacity-0": status }
+              )}
+            ></span>
+            <span
+              aria-hidden="true"
+              className={classNames(
+                "block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out",
+                { "-rotate-45": status },
+                { "translate-y-1.5": !status }
+              )}
+            ></span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Burger;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import Accordion from "./NavbarAccordion";
 import Searchbar from "../Searchbar";
+import Image from "next/image";
+import Burger from "../Burger";
 
 let colors = [
   "rgba(83, 94, 12, 1)",
@@ -20,23 +22,84 @@ const Navbar = () => {
 
   useEffect(() => {
     if (toggleMenu) setBgColor(colors[Math.floor(Math.random() * 5)]);
+    console.log(toggleMenu);
   }, [toggleMenu]);
   return (
     <>
       <div
         className={classNames(
-          "h-full items-end grid grid-cols-3 grid-rows-1 gap-x-5",
-          "md:grid-cols-12",
-          "lg:col-span-12 lg:text-3xl relative"
+          "h-full grid grid-cols-4 grid-rows-1 gap-x-4 items-end",
+          "md:grid-cols-8 md:text-md",
+          "lg:col-span-12 lg:text-lg relative"
         )}
       >
-        <h1
-          className={classNames("col-span-2 cursor-pointer font-bold", "", "")}
+        <div
+          className={classNames(
+            "col-span-3 cursor-pointer font-bold w-40 h-4",
+            "md:col-span-2 md:h-auto md:w-full",
+            ""
+          )}
         >
           <Link href="/">
-            <a>saatenberg</a>
+            <svg
+              width="205"
+              height="21"
+              viewBox="0 0 205 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-full h-full max-h-6"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M41.9691 20.3402L49.868 0H54.9501L63.0459 20.3402H41.9691ZM48.5482 16.3819L52.3606 5.83949L56.3092 16.3819H48.5482Z"
+                fill="black"
+              />
+              <path
+                d="M11.2318 13.7547C11.2318 17.9739 8.33608 20.4181 4.00717 20.4181H0V15.9079H3.56843C5.29414 15.9079 6.55186 14.5986 6.55186 12.7945V7.46968C6.55186 2.90138 9.44755 0.0498245 14.1567 0.0498245H17.7251V4.55993H14.2152C12.3432 4.55993 11.0855 5.86932 11.2318 7.67336V13.7547Z"
+                fill="black"
+              />
+              <path
+                d="M75.3313 4.55993V20.4181H70.6514V4.55993H63.7778V0.0498245H82.2049V4.55993H75.3313Z"
+                fill="black"
+              />
+              <path
+                d="M100.4 15.9079V20.4181H87.4423V0.0498245H100.4V4.55993H92.1222V7.90614H98.9373V12.4162H92.1222V15.9079H100.4Z"
+                fill="black"
+              />
+              <path
+                d="M111.068 4.55993V20.4181H106.388V0.0498245H115.894C120.457 0.0498245 123.645 3.22145 123.645 7.76065V20.4181H118.965V8.25531C118.965 6.1021 117.474 4.55993 115.455 4.55993H111.068Z"
+                fill="black"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M143.149 14.1621C143.149 17.7702 140.458 20.4181 136.743 20.4181H130.133V0.0498245H136.743C140.37 0.0498245 143.002 2.6395 143.002 6.16029C143.002 7.67336 142.476 9.01184 141.277 10.0303C142.593 11.136 143.149 12.5326 143.149 14.1621ZM136.538 4.55993H134.813V7.90614H136.538C137.562 7.90614 138.323 7.1787 138.323 6.21849C138.323 5.25827 137.562 4.55993 136.538 4.55993ZM136.538 12.2708H134.813V15.9079H136.538C137.65 15.9079 138.469 15.1223 138.469 14.0748C138.469 13.0273 137.65 12.2708 136.538 12.2708Z"
+                fill="black"
+              />
+              <path
+                d="M161.979 15.9079V20.4181H149.021V0.0498245H161.979V4.55993H153.701V7.90614H160.516V12.4162H153.701V15.9079H161.979Z"
+                fill="black"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M181.656 6.30578C181.656 9.30282 179.93 11.2814 177.327 11.9798L182.036 15.3842V21L172.647 14.0166V20.4181H167.967V0.0498245H175.104C178.907 0.0498245 181.656 2.69769 181.656 6.30578ZM176.684 6.59675C176.684 5.34556 175.748 4.41444 174.461 4.41444H172.647V8.77907H174.461C175.748 8.77907 176.684 7.84795 176.684 6.59675Z"
+                fill="black"
+              />
+              <path
+                d="M205 8.05163V20.4181H197.249C191.37 20.4181 186.69 15.8497 186.69 10.2339C186.69 4.61813 191.37 0.0498245 197.249 0.0498245H205V4.55993H197.395C194.207 4.55993 191.721 7.12051 191.721 10.2339C191.721 13.3474 194.207 15.9079 197.395 15.9079H200.32V12.2708H195.026V8.05163H205Z"
+                fill="black"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M26.2104 0L18.3115 20.3402H39.3883L31.2925 0H26.2104ZM28.703 5.83949L24.8906 16.3819H32.6516L28.703 5.83949Z"
+                fill="black"
+              />
+            </svg>
           </Link>
-        </h1>
+        </div>
         <h1 className={classNames("hidden ", "", "lg:block col-span-2")}>
           {router.asPath === "/" ? "home" : router.asPath.split("/")[1]}
         </h1>
@@ -52,8 +115,8 @@ const Navbar = () => {
         <ul
           className={classNames(
             "hidden z-10",
-            "",
-            "lg:block col-start-11 col-span-1"
+            "md:block md:col-start-6 md:col-span-1",
+            "lg:col-start-11 "
           )}
         >
           {router.locales?.map((locale) => (
@@ -64,12 +127,46 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <h1
-          className="cursor-pointer col-start-12"
-          onClick={() => setToggleMenu(true)}
+        <div
+          className="cursor-pointer col-start-12 relative bg-none"
+          style={{ zIndex: 100 }}
         >
-          menu
-        </h1>
+          {/* BURGER MENU */}
+          <div className="relative sm:max-w-xl mx-auto w-4 md:w-5 lg:w-6">
+            <nav>
+              <div
+                className="text-gray-500 w-4 md:w-5 lg:w-6 h-4 md:h-5 lg:h-6 relative focus:outline-none"
+                onClick={() => setToggleMenu(!toggleMenu)}
+              >
+                <div className="flex flex-col justify-center items-center w-full h-full absolute left-0 bottom-0">
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      "block absolute h-0.5 w-full bg-current transform transition duration-500 ease-in-out",
+                      { "rotate-45": toggleMenu },
+                      { "-translate-y-2": !toggleMenu }
+                    )}
+                  ></span>
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      "block absolute  h-0.5 w-full bg-current   transform transition duration-500 ease-in-out",
+                      { "opacity-0": toggleMenu }
+                    )}
+                  ></span>
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      "block absolute  h-0.5 w-full bg-current transform  transition duration-500 ease-in-out",
+                      { "-rotate-45": toggleMenu },
+                      { "translate-y-2": !toggleMenu }
+                    )}
+                  ></span>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
       </div>
       <div
         className={classNames(
@@ -114,12 +211,6 @@ const Navbar = () => {
               )}
             >
               {/* navbar */}
-              <h1
-                className="lg:mb-32 cursor-pointer text-right lg:mt-12"
-                onClick={() => setToggleMenu(false)}
-              >
-                x
-              </h1>
               <ul className="space-y-4 text-right">
                 <li
                   onClick={() => setToggleMenu(false)}
