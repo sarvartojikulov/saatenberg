b70942747f852042982df56d6ccb692d18f7add9
Sarvar Tojikulov <sarvar3099@gmail.com>
2021-12-22 21:33:31 +0100

    fix: style fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,27 +11,25 @@ const Home: NextPage = () => {
     <motion.div
       className={classNames(
         "grid grid-cols-4 auto-rows-min gap-5 pt-16 h-full",
-        "md:text-md md:grid-cols-8 md:gap-y-6",
+        "md:grid-cols-8 md:gap-y-6",
         "lg:grid-cols-12 lg:gap-y-8"
       )}
     >
-      <div className="col-span-full normal-case flex flex-col lg:flex-row justify-between md:mb-16 lg:mb-12">
-        <h1 className="normal-case w-full lg:w-1/2 text-md xl:text-lg leading-7 md:leading-8">
-          Saatenberg has been trading food products all over the world for 10
-          years.
-        </h1>
-        <h1 className="normal-case w-full lg:w-1/2 text-md xl:text-lg leading-7 md:leading-8">
-          Saatenberg's main focus is on the grain, spice and animal feed trade.
-          Also, dried vegetables and herbs, nuts and various oils are regularly
-          present in our turnover. It's about the details - our philosophy.
-        </h1>
-      </div>
+      <h1 className="col-span-full md:col-span-full lg:col-span-6 normal-case w-full text-md xl:text-lg leading-7 md:leading-8">
+        Saatenberg has been trading food products all over the world for 10
+        years.
+      </h1>
+      <h1 className="col-span-full md:col-span-full lg:col-span-6 md:-mt-6 lg:m-0 normal-case w-full text-md xl:text-lg leading-7 md:leading-8">
+        Saatenberg's main focus is on the grain, spice and animal feed trade.
+        Also, dried vegetables and herbs, nuts and various oils are regularly
+        present in our turnover. It's about the details - our philosophy.
+      </h1>
       <motion.div
         exit={{ x: -1000 }}
         className={classNames(
           "bg-pink-800 text-white h-44 col-span-full",
-          "md:h-80",
-          "lg:col-span-6 lg:h-96 col-span-6"
+          "md:h-80 md:col-span-full",
+          "lg:col-span-6 lg:h-96"
         )}
         transition={{ duration: 1, type: "spring" }}
       >
@@ -50,8 +48,8 @@ const Home: NextPage = () => {
         transition={{ duration: 1, type: "spring" }}
         className={classNames(
           "bg-yellow-700 text-white h-44 col-span-full",
-          "md:h-80 md:mb-4",
-          "lg:col-span-6 lg:h-96 col-span-6 lg:m-0"
+          "md:h-80 md:mb-4 md:col-span-full",
+          "lg:col-span-6 lg:h-96 lg:m-0"
         )}
       >
         <Link href="/products/organic?productID=1">
